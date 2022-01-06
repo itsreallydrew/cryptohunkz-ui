cf3dd56fc329244c0ed8c0a62a3b8cfdeeef49b9
itsreallydrew <a.mclean2526@gmail.com>
2022-01-05 21:11:35 -0600

    rough draft of layout #1

--- src/components/Roadmap/Roadmap.jsx
+++ src/components/Roadmap/Roadmap.jsx
@@ -1,15 +1,52 @@
-import React from 'react'
+import React from 'react';
 
 const Roadmap = () => {
-    return (
-        <div>
-            <h4 className="roadmap-item">Pamp Level 1: Mint out</h4>
-            <h4 className="roadmap-item">Pamp Level 2: Acquire 6x6 land plot in Sandbox</h4>
-            <h4 className="roadmap-item">Pamp Level 3: 3D VX</h4>
-            <h4 className="roadmap-item">Pamp Level 4: Building arena on our land to host events, games, concerts, meetups, etc...</h4>
-            <h4 className="roadmap-item">Pamp Level 5: Airdrop character items</h4>
-        </div>
-    )
-}
+	return (
+		<div className='roadmap'>
+			<div className='roadmap-item'>
+				<h4>Pamp Level 1: Mint out</h4>
+				<p>
+					CryptoHunkz will release their exclusive collection of 5557 #Hunkz
+					featuring 500 Animated/ Rares and 16 Legendary Hunkz 2D Art.
+				</p>
+			</div>
+			<div className='roadmap-item'>
+				<h4>Pamp Level 2: Acquire 6x6 land plot in Sandbox</h4>
+				<p>
+					The future of CryptoHunkz lies within the Metaverse. After mint out
+					the team will purchase a 6x6 lot in the #SandBox for what's to come
+					next in our building stage.
+				</p>
+			</div>
+			<div className='roadmap-item'>
+				<h4>Pamp Level 3: 3D VX</h4>
+				<p>
+					In the Metaverse CryptoHunkz will be animated into a 3D Voxel that
+					grants you access to our land and exclusive events as well as receive
+					exclusive airdrops.
+				</p>
+			</div>
+			<div className='roadmap-item'>
+				<h4>
+					Pamp Level 4: Building arena on our land to host events, games,
+					concerts, meetups, etc...
+				</h4>
+				<p>
+					With the future of CryptoHunkz being a strong active community we will
+					be building an Arena on our Sandbox land to host various Events,
+					Games, Concerts, Meetups, whatever the community has in mind.
+				</p>
+			</div>
+			<div className='roadmap-item'>
+				<h4>Pamp Level 5: Airdrop character items</h4>
+				<p>
+					To complement the 3D Voxels there will be future airdropped items
+					exclusively for VX holders to use and show off in the metaverse. (Ex.
+					Motorcycles, Animated Stoagies, Apparel){' '}
+				</p>
+			</div>
+		</div>
+	);
+};
 
-export default Roadmap
+export default Roadmap;
