fb218058fc083dbb2bf890f04f381a36230f9814
itsreallydrew <a.mclean2526@gmail.com>
2022-03-07 15:00:50 -0600

    adjusted mobile display of hunkz. fixed z-index on discord button

--- src/components/Hunkz/Hunkz.jsx
+++ src/components/Hunkz/Hunkz.jsx
@@ -11,29 +11,31 @@ const Hunkz = () => {
 	// On click function placed on each button that sets the displayed image to the selected image
 	return (
 		<div className='hunkz-display'>
-			<div className='hunkz-title'>Hunkz</div>
-			<div className='hunkz-card'>
-				<div className='hunkz-img'>
-					<img src={selected.image} alt='' />
+			<div className='hunkz-wrapper'>
+				<div className='hunkz-title'>Hunkz</div>
+				<div className='hunkz-card'>
+					<div className='hunkz-img'>
+						<img src={selected.image} alt='' />
+					</div>
+					<p>{selected.description}</p>
 				</div>
-				<p>{selected.description}</p>
-			</div>
-			<div className='hunkz-selector'>
-				<button onClick={() => setSelected(hunkz[0])} id='skin'>
-					Humanoid
-				</button>
-				<button onClick={() => setSelected(hunkz[1])} id='alien'>
-					Alien
-				</button>
-				<button onClick={() => setSelected(hunkz[2])} id='cyborg'>
-					Cyborg
-				</button>
-				<button onClick={() => setSelected(hunkz[3])} id='zombie'>
-					Zombie
-				</button>
-				<button onClick={() => setSelected(hunkz[4])} id='legend'>
-					Legendary
-				</button>
+				<div className='hunkz-selector'>
+					<button onClick={() => setSelected(hunkz[0])} id='skin'>
+						Humanoid
+					</button>
+					<button onClick={() => setSelected(hunkz[1])} id='alien'>
+						Alien
+					</button>
+					<button onClick={() => setSelected(hunkz[2])} id='cyborg'>
+						Cyborg
+					</button>
+					<button onClick={() => setSelected(hunkz[3])} id='zombie'>
+						Zombie
+					</button>
+					<button onClick={() => setSelected(hunkz[4])} id='legend'>
+						Legendary
+					</button>
+				</div>
 			</div>
 		</div>
 	);
